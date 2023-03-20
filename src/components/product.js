63cc45a94f1efc130073d60de173b510d4585f50
Sagar Bansal <sagban@Sagars-MacBook-Air.local>
2023-03-20 17:18:52 +0530

    almost done

--- src/components/product.js
+++ src/components/product.js
@@ -10,14 +10,15 @@ const Product = (props) => {
         <img width={200} src={props.product.image}></img>
         <p class="g-font-size-16--xs g-font-weight--600 g-color--dark g-margin-b-0--xs">{props.product.title}</p>
         <p class="g-font-size-14--xs g-color--dark g-margin-b-0--xs">Rs. {props.product.price}</p>
-        <a onClick={()=>handlePlay(props.product.color)} target="_blank" class="g-font-size-14--xs">
-            <b>Listen</b>
-            <span class="g-margin-l-5--xs material-icons-outlined">play_arrow</span>
-        </a>
-        <a href={props.product.link} target="_blank" class="g-margin-l-20--xs g-font-size-14--xs">
+        <a href={props.product.link} target="_blank" class="g-font-size-14--xs">
             <b>Try Now</b>
             <span class="g-margin-l-5--xs material-icons-outlined">east</span>
         </a>
+        <div class="g-margin-t-10--xs g-padding-x-10--xs g-padding-y-5--xs" onClick={()=>handlePlay(props.product.color)} 
+        style={{"width": "200px", "height": "36px", "backgroundColor": props.product.color}}>
+            <b>Listen</b>
+            <span class="g-margin-l-5--xs material-icons-outlined">play_arrow</span>
+        </div>
     </div>)
 }
 
